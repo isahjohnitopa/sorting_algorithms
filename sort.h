f6711dc18e748833a83c488d4ecdd5dc1db2491d
isahjohna <ijohnabdulmalik@gmail.com>
2023-09-24 21:10:17 +0100

    Bubble sort

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
+
 /***************************** Data Structure *******************************/
 /**
  * struct listint_s - Doubly linked list node
@@ -14,10 +17,23 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-} listint_t
+} listint_t;
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /************************* Functions Prototypes *****************************/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */

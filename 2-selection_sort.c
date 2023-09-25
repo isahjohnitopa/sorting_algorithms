ce46d60ecddd2e672bdb39f53b4b803de0e19261
isahjohna <ijohnabdulmalik@gmail.com>
2023-09-25 22:57:53 +0100

    updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -33,7 +33,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - i; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = array + i;
 		for (j = i + 1; j < size; j++)

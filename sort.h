b5af72bd27a1c4d0039338e29ecb1638ed8efeee
isahjohna <ijohnabdulmalik@gmail.com>
2023-09-25 12:50:30 +0100

    updates the header file

--- sort.h
+++ sort.h
@@ -2,6 +2,8 @@
 #define SORT_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 /***************************** Data Structure *******************************/
@@ -35,5 +37,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
